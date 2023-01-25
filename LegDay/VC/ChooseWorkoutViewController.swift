5f098e618ddce041523142c54ca62ae34fb5c528
Kim-leo <ochell2@naver.com>
2023-01-25 19:22:41 +0900

    ChooseWorkoutViewController: 컬렉션뷰 생성

--- LegDay/VC/ChooseWorkoutViewController.swift
+++ LegDay/VC/ChooseWorkoutViewController.swift
@@ -21,6 +21,8 @@ class ChooseWorkoutViewController: UIViewController {
         return btn
     }()
     
+    let workoutData = WorkoutSorting()
+    
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.navigationBar.isHidden = false
     }
@@ -41,4 +43,35 @@ class ChooseWorkoutViewController: UIViewController {
         print("Tapped")
     }
 
+}
+
+extension ChooseWorkoutViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return workoutData.sampleData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? ListCell else { return UICollectionViewCell() }
+        cell.cellLabel.text = workoutData.sampleData[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(workoutData.sampleData[indexPath.row])
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width / 3 - 5
+        return CGSize(width: width, height: width)
+    }
+    
+    
 }
---
+++ LegDay/VC/ListCell.swift
@@ -0,0 +1,14 @@
+//
+//  ListCell.swift
+//  LegDay
+//
+//  Created by 김승현 on 2023/01/25.
+//
+
+import UIKit
+
+class ListCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellLabel: UILabel!
+    
+}
