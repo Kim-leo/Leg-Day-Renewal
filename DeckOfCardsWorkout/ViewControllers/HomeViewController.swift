f2fc4b57b0c978375ad5441ff9df0ad3ea43bad1
Kim-leo <ochell2@naver.com>
2024-02-25 15:30:59 +0900

    SetWorkout VC 생성

--- DeckOfCardsWorkout/ViewControllers/HomeViewController.swift
+++ DeckOfCardsWorkout/ViewControllers/HomeViewController.swift
@@ -17,15 +17,37 @@ class HomeViewController: UIViewController {
         return btn
     }()
     
+    lazy var setWorkoutVCBtn: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("운동 설정", for: .normal)
+        btn.backgroundColor = .darkGray
+        btn.setTitleColor(.white, for: .normal)
+        return btn
+    }()
     
+    lazy var stackViewForBtns: UIStackView = {
+        let sv = UIStackView()
+        sv.axis = .vertical
+        sv.alignment = .fill
+        sv.distribution = .fillEqually
+        sv.spacing = 20
+        return sv
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Home ViewController")
         
-        self.view.addSubview(startWorkoutVCBtn)
+        self.view.addSubview(stackViewForBtns)
+        [startWorkoutVCBtn, setWorkoutVCBtn].map {
+            self.stackViewForBtns.addArrangedSubview($0)
+            
+        }
+        stackViewForBtnsLayout()
+        
         startWorkoutVCBtn.addTarget(self, action: #selector(self.startWorkoutVCBtnTapped), for: .touchUpInside)
-        startWorkoutBtnLayout()
+        setWorkoutVCBtn.addTarget(self, action: #selector(self.setWorkoutVCBtnTapped), for: .touchUpInside)
+        
     }
     
 
@@ -37,13 +59,19 @@ extension HomeViewController {
         self.navigationController?.pushViewController(startWorkoutVC, animated: true)
     }
     
-    func startWorkoutBtnLayout() {
-        startWorkoutVCBtn.translatesAutoresizingMaskIntoConstraints = false
-        startWorkoutVCBtn.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        startWorkoutVCBtn.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
-        startWorkoutVCBtn.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30).isActive = true
-        startWorkoutVCBtn.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -30).isActive = true
-        startWorkoutVCBtn.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        
+    @objc func setWorkoutVCBtnTapped(_ sender: UIButton) {
+        guard let setWorkoutVC = self.storyboard?.instantiateViewController(withIdentifier: "SetWorkoutViewController") as? SetWorkoutViewController else { return }
+        self.navigationController?.pushViewController(setWorkoutVC, animated: true)
     }
+    
+    func stackViewForBtnsLayout() {
+        stackViewForBtns.translatesAutoresizingMaskIntoConstraints = false
+        stackViewForBtns.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        stackViewForBtns.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        stackViewForBtns.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30).isActive = true
+        stackViewForBtns.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -30).isActive = true
+        stackViewForBtns.heightAnchor.constraint(equalToConstant: 100).isActive = true
+    }
+
+    
 }
