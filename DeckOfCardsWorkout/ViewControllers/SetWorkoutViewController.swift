e678f7e2ddfbac1b70d614a7893879f9e7790e22
Kim-leo <ochell2@naver.com>
2024-02-25 15:29:46 +0900

    SetWorkout ViewController 및 홈화면 접속 버튼 생성

---
+++ DeckOfCardsWorkout/ViewControllers/SetWorkoutViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SetWorkoutViewController.swift
+//  DeckOfCardsWorkout
+//
+//  Created by 김승현 on 2/25/24.
+//
+
+import UIKit
+
+class SetWorkoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
