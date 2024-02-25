9cf8f2fa77e84367b17188999791e2fd240ab1df
Kim-leo <ochell2@naver.com>
2024-02-25 17:42:59 +0900

    운동 선택 화면 - 상단 운동 종목 선택하는 CollectionView 구현

---
+++ DeckOfCardsWorkout/ViewControllers/UpperCell.swift
@@ -0,0 +1,17 @@
+//
+//  UpperCell.swift
+//  DeckOfCardsWorkout
+//
+//  Created by 김승현 on 2/25/24.
+//
+
+import UIKit
+
+class UpperCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
