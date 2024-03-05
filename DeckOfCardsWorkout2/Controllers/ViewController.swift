8cab1e064b067eeab3e227274be956e9c075a17e
Kim-leo <ochell2@naver.com>
2024-03-05 17:59:32 +0900

    Initial Commit

---
+++ DeckOfCardsWorkout2/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeckOfCardsWorkout2
+//
+//  Created by 김승현 on 3/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hi")
+    }
+
+
+}
+
