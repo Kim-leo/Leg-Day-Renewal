f8f36e9911087469a82c0a7f66c128dbc82807ed
Kim-leo <ochell2@naver.com>
2024-03-05 18:15:53 +0900

    View.swift 파일 생성 및 HomeVC 랑 연결 성공

--- DeckOfCardsWorkout2/Controllers/HomeViewController.swift
+++ DeckOfCardsWorkout2/Controllers/HomeViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
+    
+    let myView = MyView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.addSubview(myView)
+        setupViewLayout()
+        
     }
 
-
+    func setupViewLayout() {
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        myView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        myView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        myView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        myView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
 }
 
--- DeckOfCardsWorkout2/VIews/View.swift
+++ DeckOfCardsWorkout2/VIews/View.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import UIKit
+
+class MyView: UIView {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemGreen
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("뷰 초기화 에러 발생!")
+    }
+}
