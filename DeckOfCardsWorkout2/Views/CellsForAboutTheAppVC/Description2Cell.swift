312f1f4d4aad56f36c27e714a9cd7d8c4346f984
Kim-leo <ochell2@naver.com>
2024-03-14 01:44:09 +0900

    AboutTheAppVC 운동 설명 화면 - 컬렉션뷰 - 셀 프로토콜 채택 함수 세팅

---
+++ DeckOfCardsWorkout2/Views/CellsForAboutTheAppVC/Description2Cell.swift
@@ -0,0 +1,17 @@
+//
+//  Description2Cell.swift
+//  DeckOfCardsWorkout2
+//
+//  Created by 김승현 on 3/14/24.
+//
+
+import UIKit
+
+class Description2Cell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
