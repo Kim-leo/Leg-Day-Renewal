4420dc3709eeb55f002cd895a9429cfe0e57ba7e
Kim-leo <ochell2@naver.com>
2024-03-15 13:41:13 +0900

    AboutTheApp DescriptionCell 샘플이미지 첨부 및 UIImageView 생성

--- DeckOfCardsWorkout2/Views/CellsForAboutTheAppVC/DescriptionCell.swift
+++ DeckOfCardsWorkout2/Views/CellsForAboutTheAppVC/DescriptionCell.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class DescriptionCell: UICollectionViewCell {
+    lazy var screenImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.image = UIImage(named: "운동설명")
+        
+        return iv
+    }()
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -18,6 +25,25 @@ class DescriptionCell: UICollectionViewCell {
     override func prepareForReuse() {
         super.prepareForReuse()
         self.backgroundColor = nil
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.contentView.addSubview(screenImageView)
+        setupLayoutForDescriptionCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupLayoutForDescriptionCell() {
+        screenImageView.translatesAutoresizingMaskIntoConstraints = false
+        screenImageView.topAnchor.constraint(equalTo: self.contentView.topAnchor).isActive = true
+        screenImageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor).isActive = true
+        screenImageView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor).isActive = true
+        screenImageView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor).isActive = true
+        
     }
 
 }
